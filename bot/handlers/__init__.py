1e06c6f79fa1ddaa0fedd7261195411e4f9cfe7a
rwawine <rubchenya.work@gmail.com>
2025-10-01 20:38:04 +0300

    Remove outdated documentation files: BUTTON_AUDIT_REPORT.md, FIX_ERRORS.md, and TODO.md to streamline project resources and improve codebase clarity.

--- bot/handlers/__init__.py
+++ bot/handlers/__init__.py
@@ -3,17 +3,15 @@
 from .registration import RegistrationHandler, setup_registration_handlers
 from .support import setup_support_handlers
 from .common import setup_common_handlers
-from .fallback import setup_fallback_handlers
-from .global_commands import setup_global_commands  # NEW
-from .fallback_fixed import setup_fixed_fallback_handlers  # FIXED VERSION
+from .global_commands import setup_global_commands
+from .fallback_fixed import setup_fixed_fallback_handlers
 
 __all__ = [
     "RegistrationHandler", 
     "setup_registration_handlers",
     "setup_support_handlers",
     "setup_common_handlers",
-    "setup_fallback_handlers",  # DEPRECATED - use setup_fixed_fallback_handlers
-    "setup_global_commands",  # NEW
-    "setup_fixed_fallback_handlers",  # FIXED VERSION
+    "setup_global_commands",
+    "setup_fixed_fallback_handlers",
 ]
 
